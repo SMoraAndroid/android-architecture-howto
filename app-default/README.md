f7e3179956c5ca1cdd39a5c2083e0dfdae7e25ca
Stephane Mora <stephane.mora.ext@francetv.fr>
2016-11-11 20:18:10 +0100

    First commit

---
+++ app-default/README.md
@@ -0,0 +1,2 @@
+# Android Architectur How To Samples
+
---
+++ app-default/src/main/java/com/smora/arch/howto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smora.arch.howto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
