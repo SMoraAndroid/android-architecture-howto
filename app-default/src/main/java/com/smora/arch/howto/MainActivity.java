244c0500b09de7c2919a40af51bade1b0f87b24e
Stephane Mora <stephane.mora.ext@francetv.fr>
2016-11-19 15:42:05 +0100

    Sync webserver start and stop on application launch

--- app-default/src/main/java/com/smora/arch/howto/MainActivity.java
+++ app-default/src/main/java/com/smora/arch/howto/MainActivity.java
@@ -3,37 +3,18 @@ package com.smora.arch.howto;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-import android.widget.Toast;
-
-import com.smora.arch.webserver.WebServer;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebServer.with(this).startAsync(startServerCallback);
+
+        ((WebView) findViewById(R.id.webview)).loadUrl("http://localhost:8080/ws/places");
     }
-
-    @Override
-    protected void onDestroy() {
-        WebServer.with(this).stopAsync();
-        super.onDestroy();
-    }
-
-    private final WebServer.StartCallback startServerCallback = new WebServer.StartCallback() {
-        public void onServerStartSucceed() {
-            ((WebView) findViewById(R.id.webview)).loadUrl("http://localhost:8080/ws/places");
-        }
-
-        public void onServerStartFailed() {
-            Toast.makeText(getApplicationContext(), "Error when starting local server", Toast.LENGTH_LONG).show();
-        }
-    };
 
 }
